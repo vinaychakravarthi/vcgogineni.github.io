eb29d5630d9d024392dfbcbdfe309a66ba207de4
vinaychakravarthi <36322305+vinaychakravarthi@users.noreply.github.com>
2026-04-03 20:16:19 +0000

    Deploying to gh-pages from @ vinaychakravarthi/vcgogineni.github.io@dd47c5e7c33f0605d563f0686dfdeb1088a37ec8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "vinaychakravarthi/vcgogineni",
+    "data-repo": "vinaychakravarthi/vcgogineni.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
